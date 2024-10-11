4ba17dad9bc525f6ab7a487276183145fe847f56
JoaoPedro1807 <129297278+JoaoPedro1807@users.noreply.github.com>
2024-10-11 11:39:40 -0300

    Add files via upload

--- ArquivosATVD/app.py
+++ ArquivosATVD/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Blueprint
-from controller import blueprint01
+from controller.controller import blueprint01
 
 app = Flask(__name__)
 app.register_blueprint(blueprint01)
--- ArquivosATVD/controller/controller.py
+++ ArquivosATVD/controller/controller.py
@@ -1,4 +1,8 @@
-from flask import Flask, Blueprint, render_template, request, url_for, redirect
-from model import addfilme, listaFilmes
+from flask import Flask, Blueprint, render_template, request, url_for, redirect, session
+from model.model import addFilme, listaFilmes
 
 blueprint01 = Blueprint("blueprint01", __name__)
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
